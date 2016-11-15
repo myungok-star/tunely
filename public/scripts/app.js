33bdd9613b900e1ec54a1e04977272bc95047b1c
myungok-star <myungok.star@gmail.com>
2016-11-14 17:56:35 -0800

    edited the function index to access the database and pull all albums, the end of step 5 in the sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,46 +1,12 @@
-/* CLIENT-SIDE JS
- *
- * You may edit this file as you see fit.  Try to separate different components
- * into functions and objects as needed.
- *
- */
+// CLIENT-SIDE JS
+//  *
+//  * You may edit this file as you see fit.  Try to separate different components
+//  * into functions and objects as needed.
 //
-//
-// /* hard-coded data! */
-// var sampleAlbums = [];
-// sampleAlbums.push({
-//     artistName: 'Ladyhawke',
-//     name: 'Ladyhawke',
-//     releaseDate: '2008, November 18',
-//     genres: ['new wave', 'indie rock', 'synth pop']
-// });
-// sampleAlbums.push({
-//     artistName: 'The Knife',
-//     name: 'Silent Shout',
-//     releaseDate: '2006, February 17',
-//     genres: ['synth pop', 'electronica', 'experimental']
-// });
-// sampleAlbums.push({
-//     artistName: 'Juno Reactor',
-//     name: 'Shango',
-//     releaseDate: '2000, October 9',
-//     genres: ['electronic', 'goa trance', 'tribal house']
-// });
-// sampleAlbums.push({
-//     artistName: 'Philip Wesley',
-//     name: 'Dark Night of the Soul',
-//     releaseDate: '2008, September 12',
-//     genres: ['piano']
-// });
-/* end of hard-coded data */
-
-//handlebars template
-
-
-
 
 $(document).ready(function() {
     console.log('app.js loaded!');
+    // var albumData = $(this).serialize();
     $.ajax({
         method: 'GET',
         url: '/api/albums',
@@ -48,19 +14,18 @@ $(document).ready(function() {
         success: handleGetAlbumSuccess,
         error: handleGetAlbumError
     })
-
-
 });
 
 function handleGetAlbumSuccess(data) {
-    var allAlbums = data.albums;
-    allAlbums.forEach(function renderOneAlbum(album) {
+    //var allAlbums = data;
+    //console.log(allAlbums)
+    data.forEach(function(album) {
         renderAlbum(album);
     });
 }
 
-function handleGetAlbumError(a, b, c) {
-    console.log(a, b, c);
+function handleGetAlbumError(data) {
+    console.log("there was an error");
 }
 
 // this function takes a single album and renders it to the page
